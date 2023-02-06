724d978e87e097d2356db169f02a77fb7ab13ecc
felix <fmontero798@gmail.com>
2023-02-06 10:20:17 +0100

    Model

---
+++ src/main/java/com/esliceu/buckets/models/File.java
@@ -0,0 +1,31 @@
+package com.esliceu.buckets.models;
+
+public class File {
+    private int id;
+    private String hash;
+    private byte[] content;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/esliceu/buckets/models/ObjecteVersions.java
@@ -0,0 +1,23 @@
+package com.esliceu.buckets.models;
+
+public class ObjecteVersions {
+    private int idObjecte;
+    private int idFile;
+    //private Date date;
+
+    public int getIdObjecte() {
+        return idObjecte;
+    }
+
+    public void setIdObjecte(int idObjecte) {
+        this.idObjecte = idObjecte;
+    }
+
+    public int getIdFile() {
+        return idFile;
+    }
+
+    public void setIdFile(int idFile) {
+        this.idFile = idFile;
+    }
+}
